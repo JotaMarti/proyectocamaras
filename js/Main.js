b620601ae44262175b4fb27023edefc7f87efa27
Juan M <juanma887@gmail.com>
2019-12-01 13:37:22 +0100

    Prueba

--- js/Main.js
+++ js/Main.js
@@ -6,8 +6,8 @@ import validaFormulario from "./Validador.js";
 import Calculadora from "./Calculos.js";
 import Render from "./Render.js";
 
-const ejecutaValidaciones = e => {
-  e.preventDefault();
+/* const ejecutaValidaciones = e => {
+  
   let validacion = new validaFormulario();
 
   const flagValidacion = validacion.validarFormulario(altura, tilt, fov);
@@ -17,7 +17,19 @@ const ejecutaValidaciones = e => {
     let renderiza = new Render();
     renderiza.imprimeTabla(tabla);
   }
-};
+}; */
 
 //Recogiendo el Evento del boton
-formulario.addEventListener("submit", ejecutaValidaciones(e));
+formulario.addEventListener("submit", event => {
+  event.preventDefault();
+
+  let validacion = new validaFormulario();
+
+  const flagValidacion = validacion.validarFormulario(altura, tilt, fov);
+  if (flagValidacion === true) {
+    let calculadora = new Calculadora();
+    let tabla = calculadora.generaTabla(parseInt(altura.value), parseInt(tilt.value), parseInt(fov.value));
+    let renderiza = new Render();
+    renderiza.imprimeTabla(tabla);
+  }
+});
